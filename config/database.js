b8f7f42edcad48d59a5669b519110e53ab2b39e7
lostsky1403 <moch22188ti@student.nurulfikri.ac.id>
2024-12-09 16:59:52 +0700

    update config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
+// import mysql
 const mysql = require("mysql2");
 
+// buat koneksi
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -7,6 +9,7 @@ const connection = mysql.createConnection({
   database: "", // name database
 });
 
+// menghubungkan ke database
 connection.connect((err) => {
   if (err) {
     console.error("Error connecting to the database:", err.message);
@@ -15,4 +18,5 @@ connection.connect((err) => {
   console.log("Connected to the database");
 });
 
+// export db
 module.exports = connection;
--- routes/transaction.js
+++ routes/transaction.js
@@ -1,4 +1,5 @@
 const express = require("express");
+// import transaction controller
 const {
   createTransaction,
   getTransactions,
@@ -6,15 +7,19 @@ const {
   deleteTransaction,
 } = require("../controllers/transactionControll");
 const verifyToken = require("../middlewares/verifyToken");
+
+// membuat object router
 const router = express.Router();
 
+// membuat routing
 router.get("/transactions", verifyToken, getTransactions);
 // filter
 router.get("/transactions?type_cat=income", verifyToken, getTransactions);
 router.get("/transactions?type_cat=expense", verifyToken, getTransactions);
 
 router.post("/transactions/add", verifyToken, createTransaction);
-router.put("/transactions/edit", verifyToken, updateTransaction); //
+router.put("/transactions/edit", verifyToken, updateTransaction); 
 router.delete("/transactions", verifyToken, deleteTransaction);
 
+// export router
 module.exports = router;
