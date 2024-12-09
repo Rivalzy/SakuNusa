31fa0d1a5b7b56c66c0f8fbec7afbbc7c1a9634c
lostsky1403 <moch22188ti@student.nurulfikri.ac.id>
2024-12-09 21:52:09 +0700

    test model

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "", // name database
+  database: "sakutest", // name database
 });
 
 // menghubungkan ke database
